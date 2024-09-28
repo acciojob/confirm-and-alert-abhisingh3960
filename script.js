4edc38a8946559df6a3dadd73e02b3cd1e9f4db0
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-09-28 22:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function askForConfirmation() {
+    let confirmation = confirm("Do you want to proceed?");
+    
+    if (confirmation) {
+        alert("You clicked OK. Proceeding...");
+    } else {
+        alert("You clicked Cancel. Exiting...");
+    }
+}
+askForConfirmation()
+
